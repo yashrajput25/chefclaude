8e0288ee0dc6649c0292c4227701f1575d0b01d0
Yash Rajput <yashrajputyr10@gmail.com>
2025-01-08 23:48:11 +0530

    Update Main.jsx
    This React component allows users to dynamically add ingredients to a list using a form. It utilizes useState to manage the ingredient list as state. The handleSubmit function prevents form reload, extracts the input value, and updates the list. The updated list is rendered as an unordered list (<ul>), with each ingredient displayed as a list item (<li>). Key features include dynamic rendering and controlled state updates.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,42 @@
+import React from "react";
 
 export default function Main(){
+const ingredients = ["Chicken", "Oregano" , "Tomatoes"]
+
+
+const ingredientListItems = ingredients.map(ingredient => (
+    <li>{ingredient}</li>
+))
+
+const [ingredientList , setIngredientList] = React.useState([])
+
+function handleSubmit(event){
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const newIngredient = formData.get("ingredient")
+    setIngredientList(prevList => [...prevList, newIngredient])
+
+}
+
     return(
         <main>
-            <form className = "add-ingredient-form">
+            <form onSubmit = {handleSubmit} className = "add-ingredient-form">
                 <input 
                 type = "text"
                 placeholder="e.g. Oregano"
-                aria-label = "Add Ingredient"/>
+                aria-label = "Add Ingredient"
+                name = "ingredient"
+                />
+                
                 <button> + Add Ingredient</button>
             </form>
+
+            <ul>
+                {ingredientList.map(ingredient=>(
+                    <li>{ingredient}</li>
+                ))}
+            </ul>
+
         </main>
     );
 }
